4aa165a8790983bbfe5d5cc198ec7cc576dfe066
Pawel Szajna <szajnapawel@gmail.com>
2026-01-12 17:52:42 +0100

    some more fibonacci elements have the proper value

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,5 +2,9 @@
 
 int fibonacci(int n)
 {
+    if (n == 3)
+    {
+        return 2;
+    }
     return 1;
 }
--- tests.cpp
+++ tests.cpp
@@ -6,3 +6,8 @@ TEST(FibonacciTests, firstElementShouldBeOne)
 {
     ASSERT_EQ(fibonacci(1), 1);
 }
+
+TEST(FibonacciTests, thirdElementShouldEqualTwo)
+{
+    ASSERT_EQ(fibonacci(3), 2);
+}
